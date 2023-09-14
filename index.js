00f31bf5a5a8bbf113648a35420f1610fae323d3
kigary <david.mirumyan@softconstruct.com>
2023-09-14 18:15:08 +0400

    ADD: input variables for better customizability
    FIX: untrimmed text issue
    
    ADD: actual pr update functionality

--- index.js
+++ index.js
@@ -21,17 +21,50 @@ async function run() {
         core.info(`Branch prefixes: ${branchPrefixes}`)
 
         const [prefix, jiraTicket] = headBranchName.split(inputs.branchPrefixSeparator);
+        if(!branchPrefixes.includes(prefix)) {
+            core.setFailed(`Branch prefix ${prefix} is not allowed. Allowed prefixes are ${branchPrefixes}`);
+            return;
+        }
         core.info(`Prefix: ${prefix}`);
         const jiraTicketRegex = new RegExp(inputs.jiraTicketRegex);
         core.info(`Jira ticket regex: ${jiraTicketRegex}`);
-        if(jiraTicket) {
-            const [_, jiraTicketNumber, jiraTicketSummary] = jiraTicket.split(jiraTicketRegex);
-            core.info(`Jira ticket number: ${jiraTicketNumber}`);
-            core.info(`Jira ticket summary: ${jiraTicketSummary}`);
-            const jiraTicketSummaryNormalized = jiraTicketSummary.replace(new RegExp(inputs.jiraTicketSeparator), ' ').toLowerCase();
-            const jiraTicketUrl = `[${jiraTicketNumber}](https://betconstruct.atlassian.net/browse/${jiraTicketNumber})`;
-            core.info(`Jira ticket url: ${jiraTicketUrl}`);
-            core.info(`Jira ticket summary normalized: ${jiraTicketSummaryNormalized}`);
+        if (!jiraTicket) {
+            core.info(`Jira ticket is not found in branch name`);
+            return;
+        }
+        const [_, jiraTicketNumber, jiraTicketSummary] = jiraTicket.split(jiraTicketRegex);
+        core.info(`Jira ticket number: ${jiraTicketNumber}`);
+        core.info(`Jira ticket summary: ${jiraTicketSummary}`);
+        const jiraTicketSummaryNormalized = jiraTicketSummary
+          .replace(new RegExp(inputs.jiraTicketSeparator, 'gi'), ' ').trim().toLowerCase();
+        const jiraTicketUrl = `[${jiraTicketNumber}](https://betconstruct.atlassian.net/browse/${jiraTicketNumber})`;
+        core.info(`Jira ticket url: ${jiraTicketUrl}`);
+        core.info(`Jira ticket summary normalized: ${jiraTicketSummaryNormalized}`);
+
+        const title = `[${jiraTicketNumber}] ${jiraTicketSummaryNormalized}`;
+        core.info(`Title: ${title}`);
+        const initialBody = github.context.payload.pull_request.body;
+        const body =
+          ```This PR is related to ${jiraTicketUrl}
+        
+             ${initialBody}
+          ```;
+        core.info(`Body: ${body}`);
+
+        const request = {
+            owner: github.context.repo.owner,
+            repo: github.context.repo.repo,
+            pull_number: github.context.payload.pull_request.number,
+            title,
+            body
+        }
+
+        const octokit = github.getOctokit(inputs.token);
+        const response = await octokit.pulls.update(request);
+
+        core.info(`Response: ${response.status}`);
+        if (response.status !== 200) {
+            core.error('Updating the pull request has been failed');
         }
     } catch (error) {
         core.setFailed(error.message);
